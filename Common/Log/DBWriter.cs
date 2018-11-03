0c3ed71dae1ae3b3b29ca33a4c6bf7aebbf03cfd
谭小康 <799839351@qq.com>
2018-11-03 18:52:37 +0800

    Nothing

---
+++ Common/Log/DBWriter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Log
+{
+    public class DBWriter : ILogWriter
+    {
+        public void WriteLog(string ex)
+        {
+
+        }
+    }
+}
